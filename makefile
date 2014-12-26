41d99c984608ee2717df42051a4d75b305574efd
revanthb3000 <revanthb3000@gmail.com>
2014-12-26 16:31:53 -0500

    Changed the directory structure a bit and added comments. Foreign player filenames doesn't seem to be an issue on linux.

--- makefile
+++ makefile
@@ -1,2 +1,8 @@
 all:
-	cd src; python main.py
+	@echo 'Use one of the targets - crawl/postprocess'
+
+crawl:
+	cd src/Crawler; python main.py
+
+postprocess:
+	python src/PostProcessing/postProcessDataFiles.py
--- src/PostProcessing/postProcessDataFiles.py
+++ src/PostProcessing/postProcessDataFiles.py
@@ -1,5 +1,6 @@
 """
-This is a simple quick script to filter the scraped data for FIFA 13 and FIFA 15
+This is a simple quick script to filter the scraped data for FIFA 13 and FIFA 12
+The cost of player cards for various platforms was part of the play info divison and that's creeped into these data files.
 """
 import os
 
@@ -7,8 +8,26 @@ def filterData(fifaVersion):
     inputDir = "data/" + str(fifaVersion)
     outputDir = "data/" + str(fifaVersion) + "-filtered"
     for playerFile in os.listdir(inputDir):
+        #An important point to be noted here is that few files could have foreign names. Windows seems to be struggling with that.
+        print playerFile
         filterFile(inputDir + "/" + playerFile,outputDir + "/" + playerFile)
 
+"""
+This gets rid of the player prices on various platforms and converts the existing files to something similar to FIFA 14 and 15
+Stuff like this:
+
+Line # | Content
+--------------------
+2      |Full Name PS3 193,071
+3      |Xbox 148,087
+4      |PC 295,509
+5      |Wayne Rooney
+6      |Club Manchester United
+7      |League Barclays PL
+--------------------
+
+Lines 2,3,4,5 need to be merged into one line - 'Full Name Wayne Rooney'
+"""
 def filterFile(inputFileName, outputFileName):
     lines = []
     reader = open(inputFileName,"r")
